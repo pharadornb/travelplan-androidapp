111fa29862dae9b6a097afeb2ef205c7903da028
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-02-03 12:03:46 +0700

    Initial commit 03/02/2564 12.03

---
+++ app/src/main/java/com/project/travelplan/Signup.java
@@ -0,0 +1,108 @@
+package com.project.travelplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class Signup extends AppCompatActivity {
+
+    EditText txtfullname, txtusername, txtpassword, txtemail;
+    Button btnsignup;
+    TextView txtnexttologin;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        txtfullname = findViewById(R.id.fullname);
+        txtusername = findViewById(R.id.username);
+        txtpassword = findViewById(R.id.password);
+        txtemail = findViewById(R.id.email);
+        btnsignup = findViewById(R.id.btnsignup);
+        txtnexttologin = findViewById(R.id.nexttologin);
+        progressBar = findViewById(R.id.progress);
+
+        txtnexttologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent =  new Intent(Signup.this, Login.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String fullname, username, password, email;
+                fullname = String.valueOf(txtfullname.getText());
+                username = String.valueOf(txtusername.getText());
+                password = String.valueOf(txtpassword.getText());
+                email = String.valueOf(txtemail.getText());
+
+                if(!fullname.equals("") && !username.equals("") && !password.equals("") && !email.equals("")){
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[2] = "password";
+                            field[3] = "email";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = fullname;
+                            data[1] = username;
+                            data[2] = password;
+                            data[3] = email;
+                            PutData putData = new PutData("http://192.168.1.166/TravelPlaningProject_2021/API/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Sign Up Success")){
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent LoginIntent = new Intent(getApplicationContext(), Login.class);
+                                        startActivity(LoginIntent);
+                                        finish();
+                                    }else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    }); //handler
+                }else{
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
